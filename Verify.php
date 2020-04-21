f6e8572bd36a33bce3bc804dbcc012f57df3be64
Lucas D Hedding <lucashedding@1463982.no-reply.drupal.org>
2020-04-21 11:12:33 -0600

    fix phpcs

--- Verify.php
+++ Verify.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Drupal\Composer\Plugin\AutomaticUpdates;
 
 use Composer\Composer;
@@ -19,46 +21,53 @@ use Drupal\Signify\Verifier;
  *
  * @package Drupal\Composer\Plugin\AutomaticUpdates\Verify.
  */
-class Verify implements PluginInterface, EventSubscriberInterface {
+class Verify implements PluginInterface, EventSubscriberInterface
+{
 
   /**
    * @var Composer
    */
-  private $composer;
+    private $composer;
 
   /**
    * The IO interface.
    *
    * @var IOInterface
    */
-  private $io;
+    private $io;
 
   /**
    * {@inheritdoc}
    */
-  public function deactivate(Composer $composer, IOInterface $io): void {}
+    public function deactivate(Composer $composer, IOInterface $io): void
+    {
+    }
 
   /**
    * {@inheritdoc}
    */
-  public function uninstall(Composer $composer, IOInterface $io): void {}
+    public function uninstall(Composer $composer, IOInterface $io): void
+    {
+    }
 
   /**
    * {@inheritdoc}
    */
-  public function activate(Composer $composer, IOInterface $io): void {
-    $this->composer = $composer;
-    $this->io = $io;
-  }
+    public function activate(Composer $composer, IOInterface $io): void
+    {
+        $this->composer = $composer;
+        $this->io = $io;
+    }
 
   /**
    * {@inheritdoc}
    */
-  public static function getSubscribedEvents(): array {
-    return [
-      PluginEvents::POST_FILE_DOWNLOAD => 'onPostFileDownload',
-    ];
-  }
+    public static function getSubscribedEvents(): array
+    {
+        return [
+        PluginEvents::POST_FILE_DOWNLOAD => 'onPostFileDownload',
+        ];
+    }
 
   /**
    * Execute on post file download.
@@ -66,26 +75,30 @@ class Verify implements PluginInterface, EventSubscriberInterface {
    * @param \Composer\Plugin\PostFileDownloadEvent $event
    *   The post file download event.
    */
-  public function onPostFileDownload(PostFileDownloadEvent $event): void {
-    $downloader = Factory::createHttpDownloader($this->io, $this->composer->getConfig());
-    $key = file_get_contents('artifacts/keys/root.pub');
-    $verifier = new Verifier($key);
-    // @TODO: hard-code the version and package info until
-    // https://github.com/composer/composer/pull/8810 lands.
-    $url = sprintf('https://updates.drupal.org/release-hashes/%s/%s/contents-sha256sums-packaged.csig', 'drupal', '8.8.3');
-    try {
-      $csig = $downloader->get($url)->getBody();
-    } catch (TransportException $e) {
-      throw new \RuntimeException($e->getMessage(), 0, $e);
-    } catch (\Exception $e) {
-      throw new \RuntimeException('Could not read ' . $url ."\n\n" . $e->getMessage());
+    public function onPostFileDownload(PostFileDownloadEvent $event): void
+    {
+        $downloader = Factory::createHttpDownloader($this->io, $this->composer->getConfig());
+        $key = file_get_contents('artifacts/keys/root.pub');
+        $verifier = new Verifier($key);
+      // @TODO: hard-code the version and package info until
+      // https://github.com/composer/composer/pull/8810 lands.
+        $url = sprintf(
+            'https://updates.drupal.org/release-hashes/%s/%s/contents-sha256sums-packaged.csig',
+            'drupal',
+            '8.8.3'
+        );
+        try {
+            $csig = $downloader->get($url)->getBody();
+        } catch (TransportException $e) {
+            throw new \RuntimeException($e->getMessage(), 0, $e);
+        } catch (\Exception $e) {
+            throw new \RuntimeException('Could not read ' . $url . "\n\n" . $e->getMessage());
+        }
+        $files = $verifier->verifyCsigMessage($csig);
+        $checksums = new ChecksumList($files, true);
+        $failed_checksums = new FailedCheckumFilter($checksums, basename($event->getFileName()));
+        if (iterator_count($failed_checksums)) {
+            throw new \RuntimeException('The downloaded files did not match what was expected.');
+        }
     }
-    $files = $verifier->verifyCsigMessage($csig);
-    $checksums = new ChecksumList($files, TRUE);
-    $failed_checksums = new FailedCheckumFilter($checksums, basename($event->getFileName()));
-    if (iterator_count($failed_checksums)) {
-      throw new \RuntimeException('The downloaded files did not match what was expected.');
-    }
-  }
-
 }
